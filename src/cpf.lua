bf6a2cda46d91ca0748c4b9f3f91b0aaf1ba11bd
AngelsDeveloper <johnny.matos777@gmail.com>
2021-07-03 01:21:56 -0300

    Maioria das funções arrumadas e otimizadas

--- src/cpf.lua
+++ src/cpf.lua
@@ -1,5 +1,4 @@
-function SanitizarCPF(valor, max)
-    local tamanho = 0
+function SanitizarCPF(valor)
     local chars
     valor = tostring(valor)
     local stringCompleta = ''
@@ -7,17 +6,55 @@ function SanitizarCPF(valor, max)
     for i = 1, #valor do
         chars = string.sub(valor, i, i)
         if tonumber(chars) then
-            tamanho = tamanho + 1
             stringCompleta = stringCompleta .. chars
         end
     end
 
     
-    if tamanho == 11 then
+    if #stringCompleta == 11 then
         return stringCompleta
     else
         return false
     end
+end
+
+function ValidarDigito(cpf, inicio, fim, multiplicator)
+    local resto = 0
+    local soma = 0
+    local mult = multiplicator
+    local cpf_valor = string.sub(cpf, inicio, fim)
+
+    for i = 1, #cpf_valor do
+        cpf_char = string.sub(cpf_valor, i, i)
+        soma = soma + (mult * cpf_char)
+        mult = mult - 1
+    end
+
+    resto = (soma * 10) % 11
+
+    return resto
+end
+
+function FormatarCPF(cpf)
+    local chars
+    local formato = '###.###.###-##'
+    local cpfFormatado = ''
+
+    local pontos = 0
+
+    for i = 1, #formato do
+        chars = string.sub(formato, i, i)
+        chars_cpf = string.sub(cpf, i - pontos, i - pontos)
+
+        if chars == '#' then
+            cpfFormatado = cpfFormatado .. chars_cpf
+        else
+            pontos = pontos + 1
+            cpfFormatado = cpfFormatado .. chars
+        end
+    end
+
+    return cpfFormatado
 end
 
 function FormatarCPF(cpf)
@@ -49,84 +86,34 @@ function ValidarCPF(cpf)
         return false
     end
 
-    ------------------------------------------------------------------
-    ----------------------- PRIMEIRA VALIDAÇÃO -----------------------
-    ------------------------------------------------------------------
     local cpfsBlocked = {
-        [1] = '11111111111',
-        [2] = '22222222222',
-        [3] = '33333333333',
-        [4] = '44444444444',
-        [5] = '55555555555',
-        [6] = '66666666666',
-        [7] = '77777777777',
-        [8] = '88888888888',
-        [9] = '99999999999',
+        ['11111111111'] = true,
+        ['22222222222'] = true,
+        ['33333333333'] = true,
+        ['44444444444'] = true,
+        ['55555555555'] = true,
+        ['66666666666'] = true,
+        ['77777777777'] = true,
+        ['88888888888'] = true,
+        ['99999999999'] = true,
     }
 
-    for i = 1, #cpfsBlocked do
-        if cpf == cpfsBlocked[i] then
-            return false
-        end
-    end
-    
-    local array = {
-
-    }
-
-    for i = 10, 2, -1 do
-        table.insert(array, i)
-    end
-
-    local chars = 0
-    local calculo = 0
-    local ultimosdigitos = string.sub(cpf, 10, 10) .. string.sub(cpf, 11, 11)
-
-    for i = 1, 9 do
-        chars = string.sub(cpf, i , i)
-        calculo = (chars * array[i]) + calculo
-    end
-
-    resto = (calculo * 10) % 11
-    calculo = (calculo * 10) / 11
-
-    if not (tostring(resto) == string.sub(ultimosdigitos, 1, 1)) then
+    if cpfsBlocked[tostring(cpf)] then
         return false
     end
 
-    ------------------------------------------------------------------
-    ----------------------- SEGUNDA VALIDAÇÃO -----------------------
-    ------------------------------------------------------------------
+    local primeirodigito = ValidarDigito(string.sub(cpf, 1, 9), 1, 9, 10)
+    local segundodigito = ValidarDigito(string.sub(cpf, 1, 9) .. primeirodigito, 1, 10, 11) 
     
-    local array2 = {}
-    
-    for i = 11, 2, -1 do
-        table.insert(array2, i)
-    end
-    
-    calculo = 0
-    local cpf2 = ''
-    
-    for i = 1, 9 do
-        chars = string.sub(cpf, i , i)
-        cpf2 = cpf2 .. chars
-    end
+    local digitoscertos = primeirodigito .. segundodigito
 
-    cpf2 = cpf2 .. resto
+    local digitos = string.sub(cpf, 10, 10) .. string.sub(cpf, 11, 11)
 
-    for i = 1, 10 do
-        chars = string.sub(cpf2, i , i)
-        calculo = (chars * array2[i]) + calculo
-    end
-
-    resto = (calculo * 10) % 11
-    calculo = (calculo * 10) / 11
-
-    if not (tostring(resto) == string.sub(ultimosdigitos, 2, 2)) then
+    if not (digitos == digitoscertos) then
         return false
     end
+
     return FormatarCPF(cpf)
-
 end
 
 return {
